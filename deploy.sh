79d8996511e6adfa025787e5614884a84d140da8
Nicolas Martignoni <nicolas@martignoni.net>
2018-01-12 17:19:41 +0100

    Deploy script updated.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-USER=nicolas
+# Host where the server is hosted
 HOST=v22017074803151722.ultrasrv.de
+# Username of website manager on the host. No password, we use keys for authentification
+USER=nicolas
+# Directory where the site should be copied
 DIR=/var/www/moodlebox.net/html/   # might sometimes be empty!
 
+# Full URL of website
 BASEURL=https://new.moodlebox.net/
 
+# Needed to workaround some DropBox black magic :-/
 rm -rf public/
+
 hugo -b ${BASEURL} --cleanDestinationDir && rsync -avz --delete --exclude '.*' --iconv=utf-8-mac,utf-8 public/ ${USER}@${HOST}:${DIR}
 
 exit 0
